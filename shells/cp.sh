520c38e5764cd4df58fdb00b996bb42131d23dd0
luoxiaochuan <luoxiaochuan@comteck.cn>
2019-11-25 22:38:32 +0800

    add: shells/cp.sh 添加脚本同步到 blog 路径 :tada:

---
+++ shells/cp.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+function _info(){
+    echo -e "\033[32m$1\033[0m"
+}
+
+function _warning(){
+    echo -e "\033[33m$1\033[0m"
+}
+
+function _error(){
+    echo -e "\033[31m$1\033[0m"
+}
+
+function _green(){
+	echo "\033[32m"$1"\033[0m"
+}
+
+function _cyan(){
+	echo "\033[36m"$1"\033[0m"
+}
+
+function _blue(){
+	echo "\033[34m"$1"\033[0m"
+}
+
+function _magenta(){
+	echo "\033[35m"$1"\033[0m"
+}
+
+function _grey(){
+	echo "\033[37m"$1"\033[0m"
+}
+
+function _yellow(){
+	echo "\033[33m"$1"\033[0m"
+}
+
+function _red(){
+	echo "\033[31m"$1"\033[0m"
+}
+
+# 确保脚本抛出遇到的错误
+set -e
+
+_info '
+-------------------------------------
+把当前项目拷贝到 blog 同步脚本
+-------------------------------------
+'
+
+
+rsync -av --exclude  .git/ --exclude  axios/ --exclude shells/  .  ../blog/docs/axios
+
+echo
+
+cd ../blog/
+
+# git
+git pull
+git status
+git add docs/axios
+git commit -m "update: docs/axios 同步 axios-analysis :construction:"
+git push
+
+
+echo
+
+_info '
+-------------------------------------
+同步完成，并提交到远程仓库
+-------------------------------------
+'
+cd -
